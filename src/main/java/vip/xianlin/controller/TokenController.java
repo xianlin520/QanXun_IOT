91bffa80df4bf77e37a0eeff40f8d44026b901d7
XianLin <wanganning5201314@163.com>
2022-12-29 07:03:58 +0800

    优化部分配置文件, 解决打包失败问题

--- src/main/java/vip/xianlin/controller/TokenController.java
+++ src/main/java/vip/xianlin/controller/TokenController.java
@@ -5,6 +5,14 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import vip.xianlin.util.CreateVerifiCodeImage;
+
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 @RestController // 标记为控制类
 @CrossOrigin // 解决跨域问题
@@ -14,5 +22,19 @@ public class TokenController {
     @GetMapping
     public String token() {
         return "token";
+    }
+    
+    // 获取图片验证码
+    @GetMapping("/getVerifiCodeImage")
+    public void getVerifiCodeImage(HttpServletRequest request, HttpServletResponse response){
+        BufferedImage verifiCodeImage = CreateVerifiCodeImage.getVerifiCodeImage();
+        String verifiCode = new String(CreateVerifiCodeImage.getVerifiCode());
+        HttpSession session = request.getSession();
+        session.setAttribute("verifiCode",verifiCode);
+        try {
+            ImageIO.write(verifiCodeImage,"JPEG",response.getOutputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
